0dd125a3c32c545908b067d26684d243e25615f7
JuanD <dosquisd123@gmail.com>
2025-09-21 22:00:20 -0500

    feat: allow inboud traffic (ports 22 and 80)

--- entregable1/iac/terraform/main.tf
+++ entregable1/iac/terraform/main.tf
@@ -14,7 +14,6 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-
   # Specify the AWS region
   region = "us-east-2" # Ohio
 }
@@ -36,11 +35,60 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_key_pair" "app_server_key" {
+  key_name   = "los-cinco-key"
+  public_key = file("../../keys/id_rsa.pub")
+}
+
+resource "aws_security_group" "app_server_sg" {
+  name        = "los-cinco-sg"
+  description = "Security group for Los Cinco EC2 instance"
+
+  tags = {
+    Name = "Los Cinco - EC2 SG"
+  }
+
+  # Allow SSH traffic
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTP traffic
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.app_server_key.key_name
+  vpc_security_group_ids = [aws_security_group.app_server_sg.id]
+
+  associate_public_ip_address = true
 
   tags = {
     Name = "Los Cinco - EC2 App Server"
   }
 }
+
+# https://developer.hashicorp.com/terraform/cli/commands/output
+output "instance_public_ip" {
+  value       = aws_instance.app_server.public_ip
+  description = "The public IP address of the EC2 instance"
+}
+
