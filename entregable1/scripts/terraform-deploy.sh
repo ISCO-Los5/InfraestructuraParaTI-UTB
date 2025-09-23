7096c40a119c77860e7d2358e86a9bf91392f58a
JuanD <dosquisd123@gmail.com>
2025-09-23 18:47:56 -0500

    chore: skip terraform init if it has been already initialized

--- entregable1/scripts/terraform-deploy.sh
+++ entregable1/scripts/terraform-deploy.sh
@@ -1,6 +1,9 @@
 set -e
 
-terraform -chdir=./iac/terraform init
+if [ ! -d "./iac/terraform/.terraform" ]; then
+    terraform -chdir=./iac/terraform init
+fi
+
 terraform -chdir=./iac/terraform validate
 terraform -chdir=./iac/terraform plan
 terraform -chdir=./iac/terraform apply  # If needed, add -auto-approve flag
