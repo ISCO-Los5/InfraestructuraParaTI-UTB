061836f200833f2dad4e527e05bd574993150125
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2025-10-15 16:41:22 -0500

    chore: configuring tsconfig

--- III/app/src/index.ts
+++ III/app/src/index.ts
@@ -1,19 +1,40 @@
+import process from "node:process";
 import { serve } from "@hono/node-server";
+import { consola } from "consola";
 import { Hono } from "hono";
 import "dotenv/config";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-    return c.text("Hello Hono!");
+app.get("/health", (context) => {
+    return context.json({
+        ok: true,
+        timestamp: Date.now(),
+    });
 });
 
-serve(
+const server = serve(
     {
         fetch: app.fetch,
         port: 3000,
     },
     (info) => {
-        console.warn(`Server is running on http://localhost:${info.port}`);
+        consola.log(`Server is running on http://localhost:${info.port}`);
     },
 );
+
+process.on("SIGINT", () => {
+    server.close();
+    process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+    server.close((error) => {
+        if (error) {
+            consola.error(error);
+            process.exit(1);
+        }
+
+        process.exit(0);
+    });
+});
