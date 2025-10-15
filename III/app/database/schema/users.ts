7c99f70d57551aa78514e88e77627af0d416c31a
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2025-10-15 18:41:03 -0500

    feat: app was replaced

--- III/app/database/schema/users.ts
+++
@@ -1,9 +0,0 @@
-import { int, mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
-
-export const users = mysqlTable("users", () => ({
-    id: int().autoincrement().primaryKey(),
-    name: varchar({ length: 255 }).notNull(),
-    age: int().notNull(),
-    email: varchar({ length: 255 }).notNull().unique(),
-    createdAt: timestamp().defaultNow(),
-}));
--- III/app/src/routes/user/index.ts
+++
@@ -1,6 +0,0 @@
-import { Hono } from "hono";
-import { router as _get } from "@/routes/user/index.get";
-
-export const router = new Hono().basePath("/user");
-
-router.route("/", _get);
